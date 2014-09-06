12e13057f65793a3d6160a44646861e8bb8a2a86
shenzhang <shenzhang722@gamil.com>
2014-09-07 00:07:08 +1000

    ...

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * User: shenzhang
+ * Date: 9/6/14
+ * Time: 11:30 PM
+ */
+public class Main {
+    private static Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+    }
+}
